fbdf575c2591aeb6c232210c37d2e7e91b55cda7
believemaster <believemasters@gmail.com>
2022-06-06 21:29:36 +0530

    Spring forms to and MVC

---
+++ Spring MVC Form/src/main/java/com/yanik/MainController.java
@@ -0,0 +1,33 @@
+package com.yanik;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yanik.model.User;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/" )
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("userFormView");
+		User user = new User();
+		
+		modelAndView.addObject("user", user);
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/displayUserInfo")
+	public ModelAndView displayUserInfo(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView("displayUserInfo");
+		
+		System.out.println(user);
+		modelAndView.addObject("user", user);
+		
+		return modelAndView;
+	}
+}
---
+++ Spring MVC Form/src/main/webapp/WEB-INF/view/userFormView.jsp
@@ -0,0 +1,23 @@
+<%@page import="org.springframework.web.bind.annotation.ModelAttribute"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>User Form</title>
+</head>
+<body>
+	<form:form action="displayUserInfo" modelAttribute="user">
+		Name: <form:input path="name"/>
+		<hr/>
+		Gender: Male: <form:radiobutton path="gender"  value="male"/>
+						 Female: <form:radiobutton path="gender"  value="male"/>
+		<hr/>
+		<input type="submit" value="Save">
+	</form:form>
+</body>
+</html>
