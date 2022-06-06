d90c44370d635ae51ba0dfd655d7349bf84d98ec
believemaster <believemasters@gmail.com>
2022-06-06 19:47:00 +0530

    ModelAndView and foreach on data

--- Spring MVC MinimalSetup - Request Param & Model/src/main/java/com/yanik/MainController.java
+++ Spring MVC MinimalSetup - Request Param & Model/src/main/java/com/yanik/MainController.java
@@ -1,12 +1,18 @@
 package com.yanik;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -14,10 +20,32 @@ public class MainController {
 	public String welcome() {
 		return "hello";
 	}
+//	Model Approach
+//	@RequestMapping("displayName") 
+//	public String displayName(@RequestParam("firstName") String firstName, Model model) {
+//		model.addAttribute("name", firstName);
+//		return "displayName";
+//	}
 	
-	@RequestMapping("displayName") 
-	public String displayName(@RequestParam("firstName") String firstName, Model model) {
-		model.addAttribute("name", firstName);
-		return "displayName";
+//	User either Model or ModelAndView approach (Both are same)
+	
+	//	 ModelAndView Approach
+//	@RequestMapping("displayName") 			
+	@GetMapping("displayName") 				// this will only handle the get Type request
+	public ModelAndView displayName(@RequestParam("firstName") String firstName) {
+		ModelAndView modelAndView = new ModelAndView("displayName");
+		
+		Date date = new Date();
+		
+		List<String> names = new ArrayList<>();
+		
+		names.add("Jane");
+		names.add("John");
+		names.add("Judith");
+		
+		modelAndView.addObject("date", date);
+		modelAndView.addObject("name", firstName);
+		modelAndView.addObject("team", names);
+		return modelAndView;
 	}
 }
--- Spring MVC MinimalSetup - Request Param & Model/src/main/webapp/WEB-INF/view/displayName.jsp
+++ Spring MVC MinimalSetup - Request Param & Model/src/main/webapp/WEB-INF/view/displayName.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,15 @@
 <title>Display Name</title>
 </head>
 <body>
-	Hello, ${ name }
+	Hello, ${ name } , <br>
+	Today's Date is: ${date } <br>
+	___________________________________<br>
+	List of Team Members: <br>
+	<!-- For loop for list of elements here: use jstl support -->
+	<!-- ${ team }  --> <!-- this will just show array  -->
+	
+	<c:forEach var="temp" items="${ team }">
+	${ temp } <br>
+	</c:forEach>
 </body>
 </html>
